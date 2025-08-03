244ca290b4ac19e1235cbf8ea3fcde6e3efe7045
Anika Tahsin <anna.tahsin20@gmail.com>
2025-08-03 20:38:00 +0600

    Added StudentManagerUI with tabs for adding and viewing students

--- src/main/java/org/example/StudentManagerUI.java
+++ src/main/java/org/example/StudentManagerUI.java
@@ -1,9 +1,38 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class StudentManagerUI extends JFrame {
     private  JTextField nameField,banglaField,englishField,mathField;
-    private
+    private JTextArea ouputArea;
+    private JTable studenTable;
+    private DefaultTableModel tableModel;
+    private StudentManager manager=new StudentManager();
+    private final String FILE_NAME = "students.json";
+
+    public StudentManagerUI(){
+        setTitle("Student Management App");
+        setSize(1000,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setSystemLookAndFeel();
+
+        //use tabs to separate Add and Records
+        JTabbedPane tabbedPane=new JTabbedPane();
+
+        //add student Tab
+        JPanel addStudentPanel=createAddStudentPanel();
+
+        // Student Records Tab (Table + Output log)
+        JPanel recordsPanel = createRecordsPanel();
+
+        tabbedPane.addTab("Add Student",addStudentPanel);
+        tabbedPane.addTab("Studnet Records" ,recordsPanel);
+
+        add(tabbedPane);
+
+    }
 
 }
