bc2883808a936fdf5a0193f38672d2a85f2a9ae2
Anika Tahsin <anna.tahsin20@gmail.com>
2025-08-04 00:43:44 +0600

    StudentManagerTest updated.

--- src/test/java/org/example/StudentManagerTest.java
+++ src/test/java/org/example/StudentManagerTest.java
@@ -58,6 +58,24 @@ class StudentManagerTest {
         assertEquals(grade,s.getGrade(),"Grade sholud match average");
 
     }
+    @Test
+    void testTopper(){
+        manager.addStudent("bob",90,90,93);
+        manager.addStudent("tisha",60,55,78);
+        String topper=manager.getTopper();
+        assertFalse(topper.contains("tisha"),"Tisha is not topper");
+        assertTrue(topper.contains("bob"),"Bob should be topper");
+
+    }
+
+    @Test
+    void  testSameNotSame() {
+        Student s1=new Student("bob",90,90,93);
+        Student s2=new Student("tisha",60,55,78);
+        Student s3=s1;
+        assertSame(s1,s3);
+        assertNotSame(s1,s2);
 
 
+    }
 }
