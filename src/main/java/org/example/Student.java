68eff58bd814f08ac3b2d7c7e730d9a005fde582
Anika Tahsin <anna.tahsin20@gmail.com>
2025-08-03 15:12:19 +0600

    getter_setter

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,4 +1,31 @@
 package org.example;
 
 public class Student {
+    private  String name;
+    private int bangla;
+    private int english;
+    private int math;
+
+    public Student (String name,int bangla, int english,int math){
+        this.name=name;
+        this.bangla=bangla;
+        this.english=english;
+        this.math=math;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBangla() {
+        return bangla;
+    }
+
+    public void setBangla(int bangla) {
+        this.bangla = bangla;
+    }
 }
