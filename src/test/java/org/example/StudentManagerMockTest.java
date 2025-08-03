4f5843ac1f7ad40fef630e12b0c6fec73ab2229c
Anika Tahsin <anna.tahsin20@gmail.com>
2025-08-04 00:58:11 +0600

    StudentManagerMock test class updated

--- src/test/java/org/example/StudentManagerMockTest.java
+++ src/test/java/org/example/StudentManagerMockTest.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class StudentManagerMockTest {
+@Mock
+    private StudentManager manager;
+@InjectMocks
+    private StudentManager service=new StudentManager();
+
+@BeforeEach
+    void init(){
+    MockitoAnnotations.openMocks(this);
+
+}
+@Test
+    void  testMockGetTopper(){
+    when(manager.getTopper()).thenReturn("topper: Bob(273 marks)");
+    String topper=manager.getTopper();
+    assertEquals("topper: Bob(273 marks)",topper);
+    verify(manager).getTopper();
+
+}
+
 
 }
