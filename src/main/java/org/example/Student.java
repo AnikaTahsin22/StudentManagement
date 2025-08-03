ee43ad1b37e68d48f3de011e664a55e6d59d9183
Anika Tahsin <anna.tahsin20@gmail.com>
2025-08-03 15:42:55 +0600

    pom.xml file updated

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,46 +1,50 @@
 package org.example;
 
 public class Student {
-    private  String name;
+    private String name;
     private int bangla;
     private int english;
     private int math;
 
-    public Student (String name,int bangla, int english,int math){
-        this.name=name;
-        this.bangla=bangla;
-        this.english=english;
-        this.math=math;
+    public Student(String name, int bangla, int english, int math) {
+        this.name = name;
+        this.bangla = bangla;
+        this.english = english;
+        this.math = math;
     }
 
     public String getName() {
         return name;
     }
 
-
-
     public int getBangla() {
         return bangla;
     }
-
 
     public int getEnglish() {
         return english;
     }
 
-
-
     public int getMath() {
         return math;
     }
 
-
-    public int totalMarks(){
-        return bangla+english +math;
+    public int totalMarks() {
+        return bangla + english + math;
 
     }
-    public  double averageMarks(){
-        return totalMarks()/3.0;
+
+    public double averageMarks() {
+        return totalMarks() / 3.0;
+    }
+    public  String getGrade(){
+        double marks = averageMarks();
+        if (marks >= 80.0) return "A+";
+        else if (marks >=70.0) return "A";
+        else if (marks >=70.0) return "B";
+        else if (marks >=70.0) return "C";
+        else if (marks >=70.0) return "D";
+        else return  "F";
     }
 
 }
