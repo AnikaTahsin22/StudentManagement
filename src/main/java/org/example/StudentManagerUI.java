4e3a15fd9e09d7bdd86d83b54c1015aa79225c15
Anika Tahsin <anna.tahsin20@gmail.com>
2025-08-03 19:44:00 +0600

    created StudentManagerUI.java class

---
+++ src/main/java/org/example/StudentManagerUI.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import javax.swing.*;
+
+public class StudentManagerUI extends JFrame {
+    private  JTextField nameField,banglaField,englishField,mathField;
+    private
+
+}
