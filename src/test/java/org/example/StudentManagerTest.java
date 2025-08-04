d0e45d17bc5e170dbf52aa48bb573ea0ab5f9d2f
Anika Tahsin <anna.tahsin20@gmail.com>
2025-08-04 09:42:05 +0600

    ..

--- src/test/java/org/example/StudentManagerTest.java
+++ src/test/java/org/example/StudentManagerTest.java
@@ -22,6 +22,10 @@ class StudentManagerTest {
     @BeforeEach
     void setUp() {
         manager = new StudentManager();
+        manager.addStudent("Anika",80,87,90);
+        manager.addStudent("Bob", 88, 92, 75);
+        manager.addStudent("Tashnim",29,30,23);
+
     }
     @AfterEach
     void tearDown() {
@@ -30,34 +34,19 @@ class StudentManagerTest {
 
     @Test
     void testAddStudents() {
-       manager.addStudent("Anika",80,87,90);
       List<Student>students=manager.getAllStudents();
-      assertNotNull(students);
-      assertEquals(1,students.size());
+      assertEquals(3,students.size());
+
     }
+
     @Test
     void testHighestMarks(){
-        manager.addStudent("Anika",80,87,90);
-        manager.addStudent("Bob", 88, 92, 75);
-        manager.addStudent("Tashnim",29,30,23);
-        String result= manager.getHighestMarks();
-        assertTrue(result.contains("Bangla: 90"));
+
+        String highest = manager.getHighestMarks();
+        assertTrue(highest.contains("English: 92"));
 
     }
-    @ParameterizedTest
-    @ValueSource(doubles = {85.0,75.0,65.0,43.0,30.0} )
-    void testGrade(double average){
-        String grade;
-        if (average>=80 ) grade="A+";
-        else if (average >= 70) grade = "A";
-        else if (average >= 60) grade = "B";
-        else if (average >= 50) grade = "C";
-        else if (average >= 40) grade = "D";
-        else grade = "F";
-        Student s=new Student("Mahi",(int)average,(int)average,(int)average );
-        assertEquals(grade,s.getGrade(),"Grade sholud match average");
 
-    }
     @Test
     void testTopper(){
         manager.addStudent("bob",90,90,93);
@@ -76,6 +65,21 @@ class StudentManagerTest {
         assertSame(s1,s3);
         assertNotSame(s1,s2);
 
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {85.0,75.0,65.0,43.0,30.0} )
+    void testGrade(double average){
+        String grade;
+        if (average>=80 ) grade="A+";
+        else if (average >= 70) grade = "A";
+        else if (average >= 60) grade = "B";
+        else if (average >= 50) grade = "C";
+        else if (average >= 40) grade = "D";
+        else grade = "F";
+        Student s=new Student("Mahi",(int)average,(int)average,(int)average );
+        assertEquals(grade,s.getGrade(),"Grade sholud match average");
 
     }
 }
